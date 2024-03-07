f6199e372400bf34ceb9d6ad9ebfd8a3679415d8
Simon W. Jackson <hello@simonwjackson.io>
2024-03-07 15:26:16 -0600

    chore: add docker builder

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A Nix flake for gamerack including a NixOS module";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05"; # Adjust the channel as needed
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -17,8 +17,19 @@
         pkgs = nixpkgs.legacyPackages.${system};
         gamerack = import ./nix/default.nix {inherit pkgs;};
         gamerack-module = import ./nix/module.nix inputs;
+        docker = pkgs.dockerTools.buildLayeredImage {
+          name = "gamerack";
+          tag = "latest";
+          config = {
+            Cmd = ["${gamerack}/bin/gamerack"];
+          };
+          contents = [
+            gamerack
+          ];
+        };
       in {
         packages.gamerack = gamerack;
+        packages.docker = docker;
         nixosModules.default = gamerack-module;
 
         defaultPackage = gamerack;
